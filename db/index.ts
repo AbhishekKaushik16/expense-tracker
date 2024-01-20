fa5737a911b6b6d77e1b39a1f19cc2093d60efcb
AbhishekKaushik16 <abhishek.kaushik16400@gmail.com>
2024-01-21 00:24:26 +0530

    enabled query log

--- db/index.ts
+++ db/index.ts
@@ -2,4 +2,4 @@ import {drizzle} from 'drizzle-orm/vercel-postgres';
 import * as schema from "./schema";
 import {sql} from '@vercel/postgres';
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema, logger: true });
